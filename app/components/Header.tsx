4c13c840c1324398c36235d89d7a9e6f8dc97d8a
Félix Ramón <115580633+Felix9908@users.noreply.github.com>
2025-02-11 16:43:51 -0500

    update gitignore

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -11,7 +11,7 @@ export default function Header() {
         className="absolute"
       />
       <div className="absolute inset-0 bg-food-primary bg-opacity-70 flex items-center justify-center">
-        <h1 className="text-4xl md:text-6xl font-bold text-white text-center drop-shadow-lg">Sanra Barbara</h1>
+        <h1 className="text-4xl md:text-6xl font-bold text-white text-center drop-shadow-lg">Santa Barbara</h1>
       </div>
     </header>
   )
